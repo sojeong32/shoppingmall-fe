a37d7fb45c487f5b50627227285a5c6eff5a0401
박소정 <thwjd0235@naver.com>
2024-10-31 13:52:19 +0900

    feat: 상품 검색하기

--- src/page/AdminProductPage/AdminProductPage.js
+++ src/page/AdminProductPage/AdminProductPage.js
@@ -38,11 +38,19 @@ const AdminProductPage = () => {
 
   //상품리스트 가져오기 (url쿼리 맞춰서)
   useEffect(() => {
-    dispatch(getProductList());
-  }, []);
+    dispatch(getProductList({ ...searchQuery }));
+  }, [query]);
 
   useEffect(() => {
     //검색어나 페이지가 바뀌면 url바꿔주기 (검색어또는 페이지가 바뀜 => url 바꿔줌=> url쿼리 읽어옴=> 이 쿼리값 맞춰서  상품리스트 가져오기)
+    if (searchQuery.name === "") {
+      delete searchQuery.name;
+    }
+    console.log("qqq", searchQuery);
+    const params = new URLSearchParams(searchQuery);
+    const query = params.toString();
+    console.log("qqq", query);
+    navigate("?" + query);
   }, [searchQuery]);
 
   const deleteItem = (id) => {
@@ -64,6 +72,10 @@ const AdminProductPage = () => {
   const handlePageClick = ({ selected }) => {
     //  쿼리에 페이지값 바꿔주기
   };
+
+  // searchbox에서 검색어를 읽어온다 => 엔터를 치면 searchQuery객체가 업데이트 됨 {name: 스트레이트 팬츠}
+  // => searchQuery객체 안에 아이템을 기준으로 url을 새로 생성해서 호출 &name=스트레이트+팬츠
+  // => url쿼리 읽어오기 => url쿼리 기준으로 백엔드에 검색 조건과 함께 호출한다
 
   return (
     <div className="locate-center">
